ae2d0a789fffb1f193c34fcd342d7bdeb9ae5a2e
Ubuntu <ubuntu@ip-172-31-28-150.ap-northeast-1.compute.internal>
2017-09-15 10:21:13 +0000

    Fixing #43

--- imports/api/acct_module/gl.js
+++ imports/api/acct_module/gl.js
@@ -10,7 +10,7 @@ import { CallPromiseMixin } from 'meteor/didericis:callpromise-mixin';
 
 import { tableHandles } from '../../api/DBSchema/DBTOC.js';
 
-import CoA from '../DBSchema/COA.js';
+import { roundDollar } from '../helper.js';
 
 const doAutoincrement = (collection, callback) => { collection.rawCollection().findAndModify( { _id: 'autoincrement' }, [], { $inc: { value: 1 } }, { 'new': true }, callback) }
 const acctJournalNextAutoincrement = function() { return Meteor.wrapAsync(doAutoincrement) (tableHandles('acctJournal')['main']).value.value }
@@ -72,13 +72,12 @@ export const postNewJournal = new ValidatedMethod({
 				let balance = 0;
 				for (a of entries) {
 				//entries: [{COAId, journalDesc, EXCurrency, EXRate, EXAmt, supportDoc, relatedDocType, relatedDocId}]
-
 					let acct = coaList.find((v) => {
 						return v['_id'] == a['COAId']
 					})
 					if (acct===undefined) { throw new Meteor.Error('請提供正確簿記內容, 找不到科目, 或科目已被停用')}
-					if ((a.EXRate == 0) || (a.EXAmt == 0)) { throw new Meteor.Error('請提供正確簿記內容, 金額或匯率不能為 0') }
-					balance = balance + (a.EXRate * a.EXAmt * (acct.isDebit ? 1 : -1))
+					let amt = roundDollar(a.EXRate * a.EXAmt);
+					balance = balance + (amt * (acct.isDebit ? 1 : -1))
 					newEntries.push({
 						COAId: a.COAId,
 						COADesc: acct.desc,
@@ -92,18 +91,20 @@ export const postNewJournal = new ValidatedMethod({
 						EXCurrency: a.EXCurrency,
 						EXRate: a.EXRate,
 						EXAmt: a.EXAmt,
-						amt: (a.EXRate * a.EXAmt),
+						amt: amt,
 						supportDoc: a.supportDoc
 					})
 				}
-				if (balance != 0) { throw new Meteor.Error('請提供正確簿記內容, Debit/Credit 不相等') }
+				if ((balance < -0.001) || (balance > 0.001)) { throw new Meteor.Error('請提供正確簿記內容, Debit/Credit 不相等') }
 
 				const projectCode = tableHandles('project')['main'].findOne(projectId).code
 				const businessCode = tableHandles('business')['main'].findOne(businessId).code
 				let u = Meteor.users.findOne(userId);
 				const userName = u.profile.firstName + ' ' + u.profile.lastName;
 
+				//newEntries prepped in validate stage
 				const batchId = acctJournalNextAutoincrement();
+				console.log(batchId);
 				for (a of newEntries) {
 					let d = Object.assign({}, {
 						batchId: batchId,
@@ -134,6 +135,89 @@ export const postNewJournal = new ValidatedMethod({
 						amt: a.amt,
 						supportDoc: a.supportDoc
 					})
+					console.log(d)
+					let q = tableHandles('acctJournal')['main'].insert(d)
+				}
+				return batchId
+			}
+			catch(err) { throw new Meteor.Error('insert-failed', err.message) }
+		}
+	}
+});
+
+export const postReverseJournal = new ValidatedMethod({
+	name: 'acctJournal.postReverseJournal',
+	mixins:  [LoggedInMixin, CallPromiseMixin],
+	checkLoggedInError: {
+		error: 'notLoggedIn',
+		message: '用戶未有登入'
+	},
+	validate({batchNo, journalDate, fiscalYear, fiscalPeriod, reason}) {
+		try {
+			const batchCount = tableHandles('acctJournal')['main'].find({batchId: batchNo}).count
+			if (batchCount < 1) { throw new Meteor.Error('記錄編號錯誤, 編號不存在') }
+
+			try {
+				check(journalDate, Date);
+			} catch(e) { throw new Meteor.Error('請正確填寫記錄日期') }
+			try { check(fiscalYear, Number) }
+			catch(e) { throw new Meteor.Error('請正確填寫會計年度') }
+			try { check(fiscalPeriod, Number) }
+			catch(e) { throw new Meteor.Error('請正確填寫會計月份') }
+			try {
+				check(reason, String);
+				if (reason.length < 5) { throw new Error() }
+			} catch(e) { throw new Meteor.Error('請正確填寫記錄原因') }
+		} catch(e) { throw new Meteor.Error(e) }
+	},
+	run({batchNo, journalDate, fiscalYear, fiscalPeriod, reason}) {
+		if (Meteor.isServer) {
+			try {
+				const batchEntries = tableHandles('acctJournal')['main'].find({batchId: batchNo}).fetch()
+				for (a of batchEntries) {
+					const batchId = acctJournalNextAutoincrement();
+					let d = Object.assign(a, {
+						batchId: batchId,
+						journalDate: ((journalDate===undefined) ? new Date() : journalDate),
+						fiscalYear: ((fiscalYear===undefined)? '2017': fiscalYear),
+						fiscalPeriod: ((fiscalPeriod===undefined)? '9': fiscalPeriod),
+						batchDesc: 'Reversal of batch #' + batchNo + ((reason===undefined)? ', reason: ' + reason : '')
+					})
+
+				}
+
+				console.log(batchId);
+				for (a of newEntries) {
+					let d = Object.assign({}, {
+						batchId: batchId,
+						batchDesc: batchDesc,
+						journalDate: journalDate,
+						userId: userId,
+						userName: userName,
+						organization: organization,
+						projectId: projectId,
+						projectCode: projectCode,
+						businessId: businessId,
+						businessCode: businessCode,
+						fiscalYear: fiscalYear,
+						fiscalPeriod: fiscalPeriod,
+						journalType: journalType,
+						COAId: a.COAId,
+						COADesc: a.COADesc,
+						COAAcctType: a.COAAcctType,
+						COAisDebit: a.COAisDebit,
+						COAsubcat1: a.COAsubcat1,
+						COAsubcat2: a.COAsubcat2,
+						relatedDocType: a.relatedDocType,
+						relatedDocId: a.relatedDocId,
+						journalDesc: a.journalDesc,
+						EXCurrency: a.EXCurrency,
+						EXRate: a.EXRate,
+						EXAmt: a.EXAmt,
+						amt: a.amt,
+						supportDoc: a.supportDoc
+					})
+					console.log(d)
 					let q = tableHandles('acctJournal')['main'].insert(d)
 				}
 				return batchId
--- imports/api/helper.js
+++ imports/api/helper.js
@@ -12,8 +12,18 @@ export const userRole2Str = (r) => {
 	for (v in r) { //v is org name
 		for (x of r[v]) { //x is role name
 			a = a + x + "@" + v + "; "
-			console.log(r, a)
 		}
 	}
 	return a;
+}
+
+export const roundDollar = (num, decimalPlaces) => {
+    var d = decimalPlaces || 4;
+    var m = Math.pow(10, d);
+    var n = +(d ? num * m : num).toFixed(8); // Avoid rounding errors
+    var i = Math.floor(n), f = n - i;
+    var e = 1e-8; // Allow for rounding errors in f
+    var r = (f > 0.5 - e && f < 0.5 + e) ?
+                ((i % 2 == 0) ? i : i + 1) : Math.round(n);
+    return d ? r / m : r;
 }
